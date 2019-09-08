c744e9906e78e20836e380ed6aebeeef21e691f8
icadir <ismailcadir@gmail.com>
2019-09-08 17:05:04 +0300

    Simple Repository

---
+++ ShopApp.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using ShopApp.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+        Category GetById(int id);
+        Category GetOne(Expression<Func<Category, bool>> filter);
+        IQueryable<Category> GetAll(Expression<Func<Category, bool>> filter);
+        void Create(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+    }
+}
